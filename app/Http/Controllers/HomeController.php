6b982987ee382ee33094e7fb1b9a5f7eac3b7b14
aungthura <aries2061@gmail.com>
2020-11-22 11:32:19 +0630

    - retrieve data from "Posts" table after creating Model "Post" (Eloquent Model) very easy

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class HomeController extends Controller
 {
     public  function index(){
-        return view('home');
+        $data = Post::orderBy('id','desc')->get();
+        return view('home',compact('data'));
     }
 }
